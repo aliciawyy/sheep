3a255faa04448dd0c1630e95c563a63c25554992
alice <rainingilove@gmail.com>
2018-09-20 12:05:18 +0200

    add example about coroutine with return value

--- notes/coroutine.py
+++ notes/coroutine.py
@@ -1,4 +1,5 @@
-from functools import wraps
+from collections import namedtuple
+
 
 def simple_coroutine(n):
     print("before")
@@ -21,6 +22,7 @@ def demo_simple_coroutine():
 
 def running_average(initial):
     """
+    A coroutine to compute running average.
 
     Examples
     --------
@@ -46,6 +48,38 @@ def running_average(initial):
         number = yield total / count
         total += number
         count += 1
+
+
+def running_average_with_return(initial):
+    """
+    A coroutine to compute running average.
+
+    Examples
+    --------
+    >>> ave = running_average_with_return(10)
+    >>> next(ave)
+    10.0
+    >>> ave.send(20)
+    15.0
+    >>> ave.send(30)
+    20.0
+    >>> ave.send(None)
+    Traceback (most recent call last):
+    ...
+    StopIteration: Result(total=60, count=3)
+    """
+    Result = namedtuple("Result", "total count")
+    total = initial
+    count = 1
+    while True:
+        # The yield here is used to produce the average to the caller, suspend
+        # the coroutine and get a value from the caller later
+        number = yield total / count
+        if number is None:
+            break
+        total += number
+        count += 1
+    return Result(total=total, count=count)
 
 
 if __name__ == "__main__":
