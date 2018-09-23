1bf04f88bdc6818ca978886e83ecb24f7f9a16b5
alice <rainingilove@gmail.com>
2018-09-23 17:44:54 +0200

    add spinner example with asyncio in python3.7

---
+++ notes/spinner/spinner_asyncio.py
@@ -0,0 +1,44 @@
+"""
+With python3.7
+"""
+
+import asyncio
+import itertools
+import sys
+import time
+
+
+async def spin(msg):
+    write, flush = sys.stdout.write, sys.stdout.flush
+    for char in itertools.cycle("|/-\\"):
+        status = char + " " + msg
+        write(status)
+        flush()
+        # move the cursor back with the backspace character \x08
+        write('\x08' * len(status))
+        await asyncio.sleep(.1)
+    write(' ' * len(status) + '\x08' * len(status))
+
+
+async def slow_function():
+    await asyncio.sleep(3)
+    return 42
+
+
+async def supervisor():
+    print(time.strftime("[%H:%M:%S]"), "supervisor starts...")
+    spin_task = asyncio.create_task(spin("Thinking"))
+    print(spin_task)
+    result = await slow_function()
+    spin_task.cancel()
+    print(time.strftime("[%H:%M:%S]"), "supervisor done.")
+    return result
+
+
+async def main():
+    result = await supervisor()
+    print("Answer:", result)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
