55c199a283229885141b24d96cf49b8d3b51b1f6
alice <rainingilove@gmail.com>
2018-09-27 10:40:22 +0200

    update execution flow note about asyncio

--- notes/spinner/spinner_asyncio.py
+++ notes/spinner/spinner_asyncio.py
@@ -22,11 +22,29 @@ async def spin(msg):
 
 
 async def slow_function():
+    print(time.strftime("[%H:%M:%S]"), "entering slow_function...")
     await asyncio.sleep(3)
     return 42
 
 
 async def supervisor():
+    """
+    The execution order of this function is exactly as a Python coroutine.
+
+    The Python async runs with one main thread in general. So the main thread
+    first executes till the line `await asyncio.sleep(.1)` of the function spin,
+    then instead of waiting inside the function spin, the control is
+    yielded back to the main thread and the execution continues to print the
+    spin_task and then to `await` the slow function. The await is composed of
+    two steps: wrap the coroutine into a Task and yield the result. Once the
+    line await in slow_function is hit, the control is again handed back to
+    the main thread.
+
+    We noticed that the spin_task and slow_function are two tasks managed by
+    the event loop. The event loop alternate the execution between the two
+    tasks.
+
+    """
     print(time.strftime("[%H:%M:%S]"), "supervisor starts...")
     # When you create a task object, it is already scheduled to run
     spin_task = asyncio.create_task(spin("Thinking"))
